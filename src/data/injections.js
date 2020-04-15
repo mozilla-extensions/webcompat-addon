49295591b806747f40232d387a21478ffa7792d6
Ksenia Berezina <kberezina@mozilla.com>
2020-04-15 12:34:51 -0400

    Bug 1625224 - Intervention for www.sixt-neuwagen.de

--- src/data/injections.js
+++ src/data/injections.js
@@ -414,7 +414,7 @@ const AVAILABLE_INJECTIONS = [
   },
   {
     id: "bug1610344",
-    platform: "android",
+    platform: "all",
     domain: "directv.com.co",
     bug: "1610344",
     contentScripts: {
@@ -437,6 +437,21 @@ const AVAILABLE_INJECTIONS = [
       types: ["script"],
     },
     customFunc: "detectSwipeFix",
+  },
+  {
+    id: "bug1625224",
+    platform: "all",
+    domain: "sixt-neuwagen.de",
+    bug: "1625224",
+    contentScripts: {
+      matches: ["*://*.sixt-neuwagen.de/*"],
+      js: [
+        {
+          file:
+            "injections/js/bug1625224-sixt-neuwagen.de-window-netscape-shim.js",
+        },
+      ],
+    },
   },
 ];
 
