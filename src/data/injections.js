e5af1e900bc0c8ed5ac4fde9e416c8edb46d6b19
Ksenia Berezina <kberezina@mozilla.com>
2020-08-10 17:56:33 -0400

    Bug 1657574 - Remove salesforce communities intervention

--- src/data/injections.js
+++ src/data/injections.js
@@ -112,43 +112,6 @@ const AVAILABLE_INJECTIONS = [
     },
   },
   {
-    id: "bug1623375",
-    platform: "android",
-    domain: "Salesforce communities",
-    bug: "1623375",
-    contentScripts: {
-      matches: [].concat(
-        [
-          "https://faq.usps.com/*",
-          "https://help.duo.com/*",
-          "https://my211.force.com/*",
-          "https://support.paypay.ne.jp/*",
-          "https://usps.force.com/*",
-          "https://help.twitch.tv/*",
-          "https://support.sonos.com/*",
-          "https://us.community.sony.com/*",
-          "https://help.shopee.ph/*",
-          "https://exclusions.ustr.gov/*",
-          "https://help.doordash.com/*",
-          "https://community.snowflake.com/*",
-          "https://tivoidp.tivo.com/*",
-          "https://cloudreadykb.neverware.com/*",
-        ],
-        InterventionHelpers.matchPatternsForTLDs(
-          "*://support.ancestry.",
-          "/*",
-          ["ca", "co.uk", "com", "com.au", "de", "fr", "it", "mx", "se"]
-        )
-      ),
-      js: [
-        {
-          file:
-            "injections/js/bug1623375-salesforce-communities-hide-unsupported.js",
-        },
-      ],
-    },
-  },
-  {
     id: "bug1551672",
     platform: "android",
     domain: "Sites using PDK 5 video",
