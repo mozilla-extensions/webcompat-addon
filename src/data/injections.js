d29be8bf7161ea37a69851b221ec1dffa605ec3c
Ksenia Berezina <kberezina@mozilla.com>
2021-04-13 15:06:36 -0400

    Bug 1702441 - Remove an intervention for binance.com

--- src/data/injections.js
+++ src/data/injections.js
@@ -405,20 +405,6 @@ const AVAILABLE_INJECTIONS = [
     },
   },
   {
-    id: "bug1690091",
-    platform: "desktop",
-    domain: "binance.com",
-    bug: "1690091",
-    contentScripts: {
-      matches: ["*://*.binance.com/*"],
-      css: [
-        {
-          file: "injections/css/bug1690091-binance.com-page-overflow.css",
-        },
-      ],
-    },
-  },
-  {
     id: "bug1694470",
     platform: "android",
     domain: "m.myvidster.com",
