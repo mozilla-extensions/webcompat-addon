373942b8861235475536c1dd1aa91b34cf4f8bd5
Dennis Schubert <dschubert@mozilla.com>
2022-09-08 17:39:24 +0200

    Bug 1788934 - Add Cisco Webex to the InstallTrigger shim list.

--- src/data/injections.js
+++ src/data/injections.js
@@ -602,6 +602,7 @@ const AVAILABLE_INJECTIONS = [
       matches: [
         "*://*.crunchyroll.com/*", // Bug 1777597
         "*://*.pixiv.net/*", // Bug 1774006
+        "*://*.webex.com/*", // Bug 1788934
         "*://business.help.royalmail.com/app/webforms/*", // Bug 1786404
         "*://www.northcountrypublicradio.org/contact/subscribe.html*", // Bug 1778382
       ],
