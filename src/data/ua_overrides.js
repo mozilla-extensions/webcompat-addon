a7f05f53607bb3ec379d70cb6c84691e6905c67d
Thomas Wisniewski <wisniewskit@gmail.com>
2023-05-31 20:46:22 -0400

    Bug 1836181 - Add override for conference.amwell.com

--- src/data/ua_overrides.js
+++ src/data/ua_overrides.js
@@ -1332,6 +1332,23 @@ const AVAILABLE_UA_OVERRIDES = [
       },
     },
   },
+  {
+    /*
+     * Bug 1836181 - UA override for conference.amwell.com
+     *
+     * The site's content is not loaded unless a Chrome UA is used.
+     */
+    id: "bug1836181",
+    platform: "all",
+    domain: "conference.amwell.com",
+    bug: "1836181",
+    config: {
+      matches: ["*://conference.amwell.com/*"],
+      uaTransformer: originalUA => {
+        return UAHelpers.getDeviceAppropriateChromeUA();
+      },
+    },
+  },
 ];
 
 module.exports = AVAILABLE_UA_OVERRIDES;
