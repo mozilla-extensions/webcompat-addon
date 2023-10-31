5d60d137e186fdf5ee666ae67b0f2d12f6d2b04b
Ksenia <kberezina@mozilla.com>
2023-10-31 14:44:24 -0400

    Bug 1862225 - Change UA override for tesco.com (#328)

--- spec/ua_helpers.spec.js
+++ spec/ua_helpers.spec.js
@@ -82,4 +82,74 @@ describe("UAHelpers", () => {
       expect(unchangedUA).toBe(VERSION_99_UA);
     });
   });
+
+  describe("capVersionToNumber function ", () => {
+    it("Caps the version to a passed number", async () => {
+      const cappedWindowsUA = UAHelpers.capVersionToNumber(WINDOWS_UA, 119);
+      expect(cappedWindowsUA).toBe(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/119.0"
+      );
+
+      const cappedMacOSUA = UAHelpers.capVersionToNumber(MACOS_UA, 118);
+      expect(cappedMacOSUA).toBe(
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:120.0) Gecko/20100101 Firefox/118.0"
+      );
+
+      const cappedAndroidUA = UAHelpers.capVersionToNumber(ANDROID_UA, 118);
+      expect(cappedAndroidUA).toBe(
+        "Mozilla/5.0 (Android 13; Mobile; rv:120.0) Gecko/120.0 Firefox/118.0"
+      );
+
+      const cappedLinuxUA = UAHelpers.capVersionToNumber(LINUX_UA, 118);
+      expect(cappedLinuxUA).toBe(
+        "Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/118.0"
+      );
+    });
+
+    it("doesn't change the version if it's the same version or less", async () => {
+      const windowsUnchanged = UAHelpers.capVersionToNumber(WINDOWS_UA, 120);
+      expect(windowsUnchanged).toBe(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0"
+      );
+
+      const unchanged = UAHelpers.capVersionToNumber(VERSION_99_UA, 119);
+      expect(unchanged).toBe(
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:99.0) Gecko/20100101 Firefox/99.0"
+      );
+    });
+  });
+
+  describe("capVersionToNumber/capRvTo109 function combination", () => {
+    it("Caps the version to a passed number and 109 rv", async () => {
+      const cappedRvWindows = UAHelpers.capRvTo109(WINDOWS_UA);
+      const cappedWindowsUA = UAHelpers.capVersionToNumber(
+        cappedRvWindows,
+        119
+      );
+      expect(cappedWindowsUA).toBe(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0"
+      );
+
+      const cappedRvMacOS = UAHelpers.capRvTo109(MACOS_UA);
+      const cappedMacOSUA = UAHelpers.capVersionToNumber(cappedRvMacOS, 118);
+      expect(cappedMacOSUA).toBe(
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/118.0"
+      );
+
+      const cappedRvAndroid = UAHelpers.capRvTo109(ANDROID_UA);
+      const cappedAndroidUA = UAHelpers.capVersionToNumber(
+        cappedRvAndroid,
+        118
+      );
+      expect(cappedAndroidUA).toBe(
+        "Mozilla/5.0 (Android 13; Mobile; rv:109.0) Gecko/120.0 Firefox/118.0"
+      );
+
+      const cappedRvLinux = UAHelpers.capRvTo109(LINUX_UA);
+      const cappedLinuxUA = UAHelpers.capVersionToNumber(cappedRvLinux, 118);
+      expect(cappedLinuxUA).toBe(
+        "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0"
+      );
+    });
+  });
 });
